c5614b7e45d9915aa3e279921ab58dbeab8d3777
Shawn Nguru <nguru.shawn@student.greenriver.edu>
2025-01-13 17:03:34 -0600

    Person and Set and String added

--- src/Person.java
+++ src/Person.java
@@ -4,50 +4,101 @@
  */
 
 public class Person {
-  // Declare a public String instance variable for the name of the person
-  // Declare a private int instance variable for the age of the person
+    // Declare a public String instance variable for the name of the person
+    public String name;
+    // Declare a private int instance variable for the age of the person
+    private int age;
+
+    // Create a constructor that takes the name and age of the person
+    // and assigns it to the instance variables
+
+    Person(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
 
 
-  // Create a constructor that takes the name and age of the person
-  // and assigns it to the instance variables
+    // Create a toString method that gives the name and age of the person
+    public String toString()
+    {
+        return name + " " + age;
+    }
 
 
-  // Create a toString method that gives the name and age of the person
+    // Implement the below public instance method "birthYear"
+    // There should NOT be any print statement in this method.
+    /**
+     * birthYear returns the year the person was born.
+     *
+     * The birth year is calculated by subtracting the person's age from currentYear
+     * that's passed in as an int. It assumes that the person's birthday has already
+     * passed this year.
+     *
+     * @param currentYear an int for the current year
+     * @return The year the person was born
+     */
 
-
-  // Implement the below public instance method "birthYear"
-  // There should NOT be any print statement in this method.
-  /**
-   * birthYear returns the year the person was born.
-   * 
-   * The birth year is calculated by subtracting the person's age from currentYear
-   * that's passed in as an int. It assumes that the person's birthday has already
-   * passed this year.
-   * 
-   * @param currentYear an int for the current year
-   * @return The year the person was born
-   */
-  // (create the instance method here)
-
+    public int birthYear(int currentYear)
+    {
+        return currentYear - age;
+    }
+}
 
   public static void main(String[] args) {
-    // Create an instance of Person
+        Person dude = new Person("Shawn",22);
+        Person girl = new Person("Alex",23);
 
-    // Create another instance of Person with a different name and age and
-    // assign it to a different variable
+        // Create another instance of Person with a different name and age and
+        // assign it to a different variable
 
-    // Print the first person
+        // Print the first person
+        System.out.println(dude);
+        // Print the second person
+        System.out.println(girl);
 
-    // Print the second person
+        // Get the name of the first person and store it in a local variable
+        String name = dude.name;
+        // Using the birthYear method, get the birth year of the first person
+        // and store it in a local variable. Input the actual current year (e.g. 2025)
+        // as the argument.
 
-    // Get the name of the first person and store it in a local variable
+        // In a separate statement, print the local variable holding the birth year.
+        int age = dude.birthYear(2025);
+        System.out.println(age);
+        System.out.println("***************************************************");
+        /**
+         * Person
+         */
 
-    // Using the birthYear method, get the birth year of the first person
-    // and store it in a local variable. Input the actual current year (e.g. 2025)
-    // as the argument.
+        // Create a float with a negative value and assign it to a variable
+        float negativeVal = -12;
 
-    // In a separate statement, print the local variable holding the birth year.
+        // Create an int with a positive value and assign it to a variable
+        int positiveVal = 3;
 
+        // Use the modulo % operator to find the remainder when the int is divided by 3
+        System.out.println(positiveVal%3);
+
+        // Use the modulo % operator to determine whether the number is even
+        // (A number is even if it has a remainder of zero when divided by 2)
+        // Use an if-else to print "Even" if the number is even and "Odd"
+        // if the number is odd.
+        if(negativeVal%2 ==0)
+        {
+            System.out.println("Even");
+        }
+        System.out.println("Odd");
+
+        // Divide the number by another number using integer division
+        float solution = positiveVal/negativeVal;
+        /*
+         * Reminder!
+         *
+         * When dividing ints, the result is rounded down.
+         * Example:
+         * 7 / 3 = 2 when performing int division
+         */
     /**
      * Terminology!
      * 
--- src/StringPractice.java
+++ src/StringPractice.java
@@ -1,32 +1,37 @@
 public class StringPractice {
   public static void main(String[] args) {
-    // Create a string with at least 5 characters and assign it to a variable
+        // Create a string with at least 5 characters and assign it to a variable
+        String five = "fifth";
 
-    // Find the length of the string
+        // Find the length of the string
+        five.length();
 
-    // Concatenate (add) two strings together and reassign the result
+        // Concatenate (add) two strings together and reassign the result
+        String ten = five + "sixth";
 
-    // Find the value of the character at index 3
+        // Find the value of the character at index 3
+        System.out.println(ten.charAt(3));
 
-    // Check whether the string contains a given substring (i.e. does the string have "abc" in it?)
+        // Check whether the string contains a given substring (i.e. does the string have "abc" in it?)
+        System.out.println(ten.contains("six"));
 
-    // Iterate over the characters of the string, printing each one on a separate line
+        // Iterate over the characters of the string, printing each one on a separate line
+        for (int i = 0; i < ten.length(); i++)
+        {
+            System.out.println(ten.charAt(i));
+        }
+        // Create an ArrayList of Strings and assign it to a variable
+        // Add multiple strings to the List (OK to do one-by-one)
+        ArrayList<String> var  = new ArrayList<>();
+        var.add("all");
+        var.add("of");
+        var.add("the");
+        var.add("string");
 
-    // Create an ArrayList of Strings and assign it to a variable
-
-    // Add multiple strings to the List (OK to do one-by-one)
-
-    // Join all of the strings in the list together into a single string separated by commas
-    // Use a built-in method to achieve this instead of using a loop
-
-    // Check whether two strings are equal
-
-    /*
-     * Reminder!
-     * 
-     * When comparing objects in Java we typically want to use .equals, NOT ==.
-     * 
-     * We use == when comparing primitives (e.g. int or char).
-     */
+        // Join all-of-the strings in the list together into a single string separated by commas
+        // Use a built-in method to achieve this instead of using a loop
+        String joined = String.join(",", var);
+        // Check whether two strings are equal
+        System.out.println(joined);
   }
 }
